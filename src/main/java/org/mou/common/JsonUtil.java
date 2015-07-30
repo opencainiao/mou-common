a2d950a523ce5512c1fd23882a450f9bacdde0d6
opencainiao <25662996@qq.com>
2015-07-30 19:15:25 +0800

    增加部分功能

--- src/main/java/org/mou/common/JsonUtil.java
+++ src/main/java/org/mou/common/JsonUtil.java
@@ -1,13 +1,19 @@
 package org.mou.common;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class JsonUtil {
 
 	private static Gson gson = new Gson();
+	private static GsonBuilder gsonBuilder = new GsonBuilder();
 
 	public static Gson getGson() {
 		return JsonUtil.gson;
+	}
+	
+	public static GsonBuilder getGsonBuilder(){
+		return gsonBuilder;
 	}
 
 	/****
@@ -40,5 +46,27 @@ public class JsonUtil {
 	public static String getFormatJsonStr(Object o) {
 		String rtnStr = toJsonStr(o);
 		return rtnStr.replaceAll(",", "\n").replaceAll("},", "\n");
+	}
+
+	/****
+	 * 获取格式化的json字符串表示
+	 * 
+	 * @param o
+	 * @return
+	 */
+	public static String getPrettyJsonStr(Object o) {
+		Gson prettyGson = getGsonBuilder().setPrettyPrinting().create();
+		return prettyGson.toJson(o);
+	}
+
+	/****
+	 * 获取包含null值的属性
+	 * 
+	 * @param o
+	 * @return
+	 */
+	public static String getIncludeNullGsonStr(Object o) {
+		Gson includeNullsGson = getGsonBuilder().serializeNulls().create();
+		return includeNullsGson.toJson(o);
 	}
 }
