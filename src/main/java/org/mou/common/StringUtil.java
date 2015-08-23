242467cf2da97dc4bdfdacb2cb1a860693a55dfa
opencainiao <25662996@qq.com>
2015-08-23 11:54:31 +0800

    添加字符串内容的校验方法

--- src/main/java/org/mou/common/StringUtil.java
+++ src/main/java/org/mou/common/StringUtil.java
@@ -3,6 +3,8 @@ package org.mou.common;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringUtil {
 
@@ -209,10 +211,80 @@ public class StringUtil {
 		return str;
 	}
 
+	/****
+	 * 判断一个字符串是否全是数字
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNumeric(String str) {
+		if (StringUtil.isEmpty(str)) {
+			return false;
+		}
+		Pattern pattern = Pattern.compile("[0-9]+");
+		Matcher isNum = pattern.matcher(str);
+		if (!isNum.matches()) {
+			return false;
+		}
+		return true;
+	}
+
+	/****
+	 * 判断一个字符串是否是小数
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isDouble(String str) {
+
+		if (StringUtil.isEmpty(str)) {
+			return false;
+		}
+
+		java.util.regex.Pattern pattern = java.util.regex.Pattern.compile("^\\d+\\.\\d+$");
+
+		java.util.regex.Matcher m2 = pattern.matcher(str);
+		if (m2.matches()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/****
+	 * 判断是否是数字或是小数
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isNumericOrDouble(String str) {
+		if (isNumeric(str) || isDouble(str)) {
+			return true;
+		}
+
+		return false;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(addCharL(1, 5, "0"));
 		System.out.println(addCharR(1, 3, "A"));
 		System.out.println(addCharL("mm", 5, "0"));
 		System.out.println(addCharR("n", 3, "A"));
+
+		System.out.println("is_double------------");
+		System.out.println(isDouble("123"));
+		System.out.println(isDouble("123.1"));
+		System.out.println(isDouble("123.10"));
+		System.out.println(isDouble("123.1.0"));
+		System.out.println("is_numeric------------");
+		System.out.println(isNumeric("123"));
+		System.out.println(isNumeric("123.1"));
+		System.out.println(isNumeric("123.10"));
+		System.out.println(isNumeric("123.1.0"));
+		System.out.println("is_numeric_or_double------------");
+		System.out.println(isNumericOrDouble("123"));
+		System.out.println(isNumericOrDouble("123.1"));
+		System.out.println(isNumericOrDouble("123.10"));
+		System.out.println(isNumericOrDouble("123.1.0"));
 	}
 }
